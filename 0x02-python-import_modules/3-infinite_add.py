c8fb2d0f3860cc206847dd15623ef9e3ffec2e36
CObitimah <obitimahcephas@gmail.com>
2024-01-19 13:35:40 +0000

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    sumint = 0
+    for i in range(1, len(argv)):
+        sumint += int (argv[1])
+    print("{}".format(sumint))
