25a4c42fac03e77d9abc95d2eb38552387ebbcc4
CObitimah <obitimahcephas@gmail.com>
2024-01-19 11:36:23 +0000

    My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, subtract, multiply,divide
+
+    a = 10
+    b = 5
+
+    print(f"{} + {} = {}".format(a, b, add(a, b)))
+    print(f"{} - {} = {}".format(a, b, subtract(a, b)))
+    print(f"{} * {} = {}".format(a, b, multiply(a, b)))
+    print(f"{} / {} = {}".format(a, b, divide(a, b)))
