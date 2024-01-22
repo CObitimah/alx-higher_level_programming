3cfdb0a0aaf41c6e709047eaf51fcd0b57664caf
CObitimah <obitimahcephas@gmail.com>
2024-01-22 15:09:36 +0000

    Secure access to an element in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 def element_at(my_list, idx):
     
     if idx < 0:
-        
         return None
+    
     for i, element in enumerate(my_list):
         if i == idx:
             return element
