0fbbf0349e7013582e9e067ea503ee030d7e491b
CObitimah <obitimahcephas@gmail.com>
2024-04-29 11:49:37 -0400

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const numArgs = process.argv.length - 2;
-if (numArgs === 0) {
-	console.log('No argument');
-} else if (numArgs === 1) {
-	console.log('Argument found');
+// Prints a message depending of the number of arguments passed
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
