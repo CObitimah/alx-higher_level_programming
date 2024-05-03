171b844b2f1b648dc7cc3a732f8d7b5c477a1a31
CObitimah <obitimahcephas@gmail.com>
2024-05-03 08:54:13 -0400

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""  Lists all states from the database hbtn_0e_0_usa """
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
@@ -14,4 +15,3 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
-
