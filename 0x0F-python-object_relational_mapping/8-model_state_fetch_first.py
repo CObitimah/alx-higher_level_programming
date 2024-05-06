2ab02bd4468df1c7316eb14bd23de1701aebfa39
CObitimah <obitimahcephas@gmail.com>
2024-05-06 13:19:14 -0400

    First state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
-"""
+
+""" script that prints the first State object."""
+
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+def list_first_state(u_name, p_word, db_name):
+    state_query = f"mysql://{u_name}:{p_word}@localhost:3306/{db_name}"
+    engine = create_engine(state_query, pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
+
     session = Session()
-    instance = session.query(State).first()
-    if instance is None:
-        print("Nothing")
-    else:
-        print(instance.id, instance.name, sep=": ")
+    first_state = session.query(State).order_by(State.id).first()
+    if not first_state:
+        print('Nothing')
+
+    print(f'{first_state.id}: {first_state.name}')
+    session.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print('Error')
+        sys.exit(1)
+
+    u_name, p_word, db_name = sys.argv[1:]
+    list_first_state(u_name, p_word, db_name)
