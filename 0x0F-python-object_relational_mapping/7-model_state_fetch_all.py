7497df3e98a2b70da9c3b39c34078f9989fe5515
CObitimah <obitimahcephas@gmail.com>
2024-05-06 13:16:01 -0400

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+
+
+"""script that lists all State objects from the database hbtn_0e_6_usa."""
+
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+def list_class(u_name, p_word, db_name):
+    query_1 = f'mysql://{u_name}:{p_word}@localhost:3306/{db_name}'
+    engine = create_engine(query_1, pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
+
+    states = session.query(State).order_by(State.id.asc()).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Erro")
+        sys.exit(1)
+
+    u_name, p_word, db_name = sys.argv[1:]
+    list_class(u_name, p_word, db_name)
