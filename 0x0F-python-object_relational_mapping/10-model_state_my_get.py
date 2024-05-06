0a20fad11bf9b8f4fbda7842652e1b9508d5916e
CObitimah <obitimahcephas@gmail.com>
2024-05-06 09:21:12 -0400

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-"""
-Script that prints the State object with the name passed as argument
-from the database
-Using module SQLAlchemy
-"""
+"""Print the State object with the name passed as an argument from the database hbtn_0e_6_usa"""
 
-from model_state import Base, State
+import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
+from sqlalchemy.orm import sessiomaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    # create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    # create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-    # create a Session
-    session = Session()
-    Base.metadata.create_all(engine)
+    # Check if correct number of arguments is provided
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database name> <state name<".format(sys.argv[0]))
+        sys.exit(1)
 
-    s_tate = session.query(State).filter(State.name == argv[4]).first()
+        # Connect to the MySQL server
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                               .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
-    if s_tate:
-        print("{}".format(s_tate.id))
-    else:
-        print("Not found")
-    session.close()
+        # Create a session to interact with the database
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # Query the State object with the given name
+        state_name = sys.argv[4]
+        state = session.query(State).filter(State.name == state_name).first()
+
+        # Print the state ID if found, otherwise print "Not found"
+        if state:
+            print(state.id)
+        else:
+            print("Not found")
+
+        # Close the session
+        session.close()
