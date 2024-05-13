f9a0af46e8cbee085c6c36c3cbaf1f948db6128a
CObitimah <obitimahcephas@gmail.com>
2024-05-13 08:36:56 -0400

    What is my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# A script that fetches url
+import urllib.request
+url = 'https://alxintranet.hbtn.io/status'
+with urllib.request.urlopen(url) as req:
+    html_content = req.read()
+    utf_data = html_content.decode('utf-8')
+    data_type = type(html_content)
+    print(f'Body response:\n\t- type\
+: {data_type}\n\t- content: {html_content}\n\t- utf8 content: {utf_data}')
