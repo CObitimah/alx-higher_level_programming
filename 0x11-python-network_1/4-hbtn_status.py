b9d34000b6052c0107e10c075e3afdbb8854ffe1
CObitimah <obitimahcephas@gmail.com>
2024-05-14 09:46:47 -0400

    What is my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 """
 Script to fetch and display information from https://alx-intranet.hbtn.io/status using the requests package.
-
-Usage:
-    ./4-hbtn_status.py
-
-Requirements:
-    - requests package
 """
-
 import requests
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
-    response = request.get(url)
-    data = response.text
+    r = requests.get(url)
+    data = r.text
     data_type = type(data)
     print(f'Body response:\n\t- type: {data_type}\n\t- content: {data}')
