d4ac5490aece43484d9f85cb2ec8ecec704b7245
CObitimah <obitimahcephas@gmail.com>
2024-05-14 12:25:49 -0400

    POST an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script to a POST request to a given URL with an email parameter and display the response body.
+"""
+import requests
+import sys
+
+def main():
+    """
+    Main function to send a POST request with the provided email address.
+    """
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    response = requests.post(url, data=payload)
+    print(response.text)
+
+if __name__ == '__main__':
+    main()
