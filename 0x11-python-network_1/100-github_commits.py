3c5017fc142dede22477a70a42f9f9c39abfe4dc
CObitimah <obitimahcephas@gmail.com>
2024-05-16 12:51:48 -0400

    Time for an interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script takes a repository name and owner name as arguments and uses the GitHub API to list 10 commits (from the most recent to oldest) of the repository by the specified owner.
+"""
+
+import sys
+import requests
+
+def get_commits(repo, owner):
+    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    response = requests.get(url)
+    if response.status_code == 200:
+        commits = response.json()[:10]  # Get the first 10 commits
+        for commit in commits:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+    else:
+        print("Error fetching commits")
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    get_commits(repo_name, owner_name)
