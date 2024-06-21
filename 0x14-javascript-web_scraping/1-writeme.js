8019de163aaa67c71711ba16f1d3c72ecbff8125
CObitimah <obitimahcephas@gmail.com>
2024-06-21 14:17:20 -0400

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,19 +1,10 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-if (!filePath || !content) {
-  console.error('Error: Missing arguments. Usage: node script.js <file-path> <content>');
-  process.exit(1);
-}
-
 fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
-    console.error(`Error writing to file: ${err.message}`);
-    process.exit(1);
-  } else {
-    console.log('File has been written successfully.');
+    console.log(err);
   }
 });
