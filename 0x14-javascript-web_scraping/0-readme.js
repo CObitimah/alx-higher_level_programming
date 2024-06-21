c6f59944be0b6f60e7b5795471753cba563f5dc3
CObitimah <obitimahcephas@gmail.com>
2024-06-21 13:51:15 -0400

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,19 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 const filePath = process.argv[2];
 
+if (!filePath) {
+  console.error('Error: No file path provided. Usage: node script.js <file-path>');
+  process.exit(1);
+}
+
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.log(err);
+    console.error(`Error reading file: ${err.message}`);
+    process.exit(1);
   } else {
     console.log(data);
   }
 });
+
