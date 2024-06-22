1440bad1e73dcb4052296ceea5c79fbd58b1abb6
CObitimah <obitimahcephas@gmail.com>
2024-06-22 03:47:49 -0400

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,33 +1,17 @@
 #!/usr/bin/node
-
-const fs = require('fs');
 const request = require('request');
-
-if (process.argv.length !== 4) {
-  console.error('Usage: ./5-request_store.js <URL> <filename>');
-  process.exit(1);
-}
-
+const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching URL:', error);
-    return;
+request.get(url, (err, response) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(filePath, response.body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
-
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch data. Status code:', response.statusCode);
-    return;
-  }
-
-  // Write the body content to the file with UTF-8 encoding
-  fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-      return;
-    }
-    console.log(`Successfully wrote content from ${url} to ${filePath}`);
-  });
 });
