b3c2a17f62af16c7426544411735429259641aad
CObitimah <obitimahcephas@gmail.com>
2024-06-22 02:58:41 -0400

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,27 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const movieId = process.argv[2];
 
 if (!movieId) {
-  console.error('Error: Missing movie ID. Usage: node script.js <movie-id>');
+  console.error('Error: Missing movie ID. Usage: ./3-starwars_title.js <movie-id>');
   process.exit(1);
 }
 
-// Construct the API URL
-const urlApi = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+const urlApi = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(urlApi, (err, response, body) => {
   if (err) {
     console.error(`Error making request: ${err.message}`);
     process.exit(1);
-  } 
-  else if (response.statusCode !== 200) {
+  } else if (response.statusCode !== 200) {
     console.error(`Error: Received status code ${response.statusCode}`);
     process.exit(1);
-  } 
-  else {
+  } else {
     try {
       const data = JSON.parse(body);
       console.log(data.title);
